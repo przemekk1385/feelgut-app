5e48675c21b8434e5fceab5772d8f5d7c56285fc
Przemysław Kaliś <przemek.kalis@gmail.com>
2023-02-22 22:15:02 +0100

    Add shadow

--- components/Reviews.vue
+++ components/Reviews.vue
@@ -2,15 +2,15 @@
   <div class="bg-[#E1E1E1] px-32 pt-12 pb-6">
     <div class="text-center">
       <span class="text-8xl font-bold text-white"> Opinie klientów </span>
-      <div class="mx-auto mt-6 h-1 w-[40%] pl-[5%]">
+      <div class="mx-auto mt-6 h-1 w-[50%] pl-[5%]">
         <div
           class="h-1 bg-gradient-to-r from-[#FCCB00] via-[#C9961A] to-[#FFFAD2]"
         ></div>
       </div>
     </div>
     <div>
-      <div class="mt-6 grid grid-cols-2 gap-12">
-        <div class="rounded-[10%/25%] bg-[#FFFCFC] px-8 pt-4 pb-8">
+      <div class="mt-8 grid grid-cols-2 gap-12">
+        <div class="rounded-[10%/25%] bg-[#FFFCFC] px-8 pt-4 pb-8 drop-shadow-[5px_5px_20px_rgba(0,0,0,.25)]">
           <div class="flex justify-center">
             <img src="images/star.svg" alt="Gwiazdka" class="mx-1" />
             <img src="images/star.svg" alt="Gwiazdka" class="mx-1" />
@@ -26,7 +26,7 @@
           </div>
         </div>
 
-        <div class="rounded-[10%/25%] bg-[#FFFCFC] px-8 pt-4 pb-8">
+        <div class="rounded-[10%/25%] bg-[#FFFCFC] px-8 pt-4 pb-8 drop-shadow-[5px_5px_20px_rgba(0,0,0,.25)]">
           <div class="flex justify-center">
             <img src="images/star.svg" alt="Gwiazdka" class="mx-1" />
             <img src="images/star.svg" alt="Gwiazdka" class="mx-1" />
