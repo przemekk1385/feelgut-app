f105497085fba6073254a7becfd3bca91bff5cbc
Przemysław Kaliś <przemek.kalis@gmail.com>
2023-02-26 11:09:33 +0100

    Static template (#1)
    * Setup fonts, colors etc.
    
    * Create Header component
    
    * Create About component
    
    * Set image cover
    
    * Replace with better quality version
    
    * Create Offer component
    
    * Adjust border, format
    
    * Create Reviews component
    
    * Fix shadow
    
    * Add shadow
    
    * Add shadow
    
    * Format
    
    * Create Contact component
    
    * Adjust shadow
    
    * Create MyMassageRoom component
    
    * Format
    
    * Create Map component
    
    * Add Footer component
    
    * Format
    
    * Use components

--- app.vue
+++ app.vue
@@ -1,5 +1,12 @@
 <template>
-  <div>
-    <NuxtWelcome />
+  <div class="w-full font-playfair-display">
+    <Header />
+    <About />
+    <Offer />
+    <Reviews />
+    <Contact />
+    <MyMassageRoom />
+    <Map />
+    <Footer />
   </div>
 </template>
---
+++ components/Reviews.vue
@@ -0,0 +1,51 @@
+<template>
+  <div class="bg-[#E1E1E1] px-32 pt-12 pb-6">
+    <div class="text-center">
+      <span class="text-8xl font-bold text-white"> Opinie klientów </span>
+      <div class="mx-auto mt-6 h-1 w-[50%] pl-[5%]">
+        <div
+          class="h-1 bg-gradient-to-r from-[#FCCB00] via-[#C9961A] to-[#FFFAD2]"
+        ></div>
+      </div>
+    </div>
+    <div>
+      <div class="mt-8 grid grid-cols-2 gap-12">
+        <div
+          class="rounded-[10%/25%] bg-[#FFFCFC] px-8 pt-4 pb-8 drop-shadow-[5px_5px_20px_rgba(0,0,0,.25)]"
+        >
+          <div class="flex justify-center">
+            <img src="images/star.svg" alt="Gwiazdka" class="mx-1" />
+            <img src="images/star.svg" alt="Gwiazdka" class="mx-1" />
+            <img src="images/star.svg" alt="Gwiazdka" class="mx-1" />
+            <img src="images/star.svg" alt="Gwiazdka" class="mx-1" />
+            <img src="images/star.svg" alt="Gwiazdka" class="mx-1" />
+          </div>
+          <div class="mt-6">
+            <span class="font-lato text-2xl"
+              ><span class="font-medium">Masaż był super, polecam każdemu</span
+              ><br /><span class="font-light">- Paweł</span></span
+            >
+          </div>
+        </div>
+
+        <div
+          class="rounded-[10%/25%] bg-[#FFFCFC] px-8 pt-4 pb-8 drop-shadow-[5px_5px_20px_rgba(0,0,0,.25)]"
+        >
+          <div class="flex justify-center">
+            <img src="images/star.svg" alt="Gwiazdka" class="mx-1" />
+            <img src="images/star.svg" alt="Gwiazdka" class="mx-1" />
+            <img src="images/star.svg" alt="Gwiazdka" class="mx-1" />
+            <img src="images/star.svg" alt="Gwiazdka" class="mx-1" />
+            <img src="images/star.svg" alt="Gwiazdka" class="mx-1" />
+          </div>
+          <div class="mt-6">
+            <span class="font-lato text-2xl"
+              ><span class="font-medium">Masaż był super, polecam każdemu</span
+              ><br /><span class="font-light">- Paweł</span></span
+            >
+          </div>
+        </div>
+      </div>
+    </div>
+  </div>
+</template>
