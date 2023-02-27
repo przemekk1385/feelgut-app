358108400fafe2680b7de57c174261a5c77ffcdc
Przemysław Kaliś <przemek.kalis@gmail.com>
2023-02-27 21:56:17 +0100

    Apply RWD

--- components/Footer.vue
+++ components/Footer.vue
@@ -1,13 +1,17 @@
 <template>
-  <div class="flex justify-around bg-como px-1/10 py-12">
-    <div class="flex items-center">
+  <div
+    class="flex flex-col justify-between bg-como px-6 py-6 md:flex-row md:flex-wrap lg:px-1/10 lg:py-12 xl:justify-around"
+  >
+    <div
+      class="mb-6 flex justify-center md:basis-full lg:mb-0 lg:basis-auto lg:items-center"
+    >
       <div>
         <a href="/">
           <img src="/images/feelgut-logo-w.svg " alt="Logo" class="w-28" />
         </a>
       </div>
     </div>
-    <div class="px-3">
+    <div class="mb-2 px-3 md:mb-0">
       <ul class="grid-col-1 grid gap-y-2 font-lato text-white">
         <li><a href="#">O mnie</a></li>
         <li><a href="#">Usługi</a></li>
@@ -15,14 +19,14 @@
         <li><a href="#">Umów wizytę</a></li>
       </ul>
     </div>
-    <div class="w-1 bg-white">&nbsp;</div>
-    <div class="flex items-center px-3">
-      <ul class="grid-col-1 grid gap-y-6 font-lato text-white">
+    <div class="my-6 h-1 w-full bg-white md:my-0 md:h-auto md:w-1">&nbsp;</div>
+    <div class="mb-2 flex items-center px-3 lg:mb-0">
+      <ul class="grid-col-1 grid gap-y-2 font-lato text-white lg:gap-y-6">
         <li><a href="#">Masaże relaksacyjne</a></li>
         <li><a href="#">Masaże terapeutyczne</a></li>
       </ul>
     </div>
-    <div class="w-1 bg-white">&nbsp;</div>
+    <div class="my-6 h-1 w-full bg-white md:my-0 md:h-auto md:w-1">&nbsp;</div>
     <div class="px-3">
       <ul class="grid-col-1 grid gap-y-2 font-lato text-white">
         <li>Telefon: 788 139 475</li>
@@ -30,9 +34,9 @@
         <li>Adres: Kolanowska 123,<br /><span class="ml-16">Bochnia</span></li>
       </ul>
     </div>
-    <div class="w-1 bg-white">&nbsp;</div>
+    <div class="my-6 h-1 w-full bg-white md:my-0 md:h-auto md:w-1">&nbsp;</div>
     <div class="px-3">
-      <ul class="flex h-full flex-col justify-around">
+      <ul class="flex h-full flex-row justify-around md:flex-col">
         <li>
           <a
             href="#"
--- components/Header.vue
+++ components/Header.vue
@@ -1,9 +1,11 @@
 <template>
   <div class="aspect-video w-full">
-    <div class="h-full w-full bg-header-image bg-cover px-1/10 pt-[2%]">
+    <div class="h-full w-full bg-header-image bg-cover px-6 pt-[2%] lg:px-1/10">
       <nav class="flex items-center justify-between">
-        <ul class="flex w-full items-center justify-between">
-          <li class="w-[18%]">
+        <ul
+          class="flex w-full flex-wrap items-center justify-around lg:justify-between"
+        >
+          <li class="hidden w-[18%] md:block">
             <a href="/" class="font-medium">
               <img src="/images/feelgut-logo.png" alt="Logo" class="h-28" />
             </a>
@@ -12,30 +14,37 @@
           <li><a href="#" class="font-medium">Usługi</a></li>
           <li><a href="#" class="font-medium">Gabinet</a></li>
           <li><a href="#" class="font-medium">Cennik</a></li>
-          <li>
+          <li class="basis-full md:basis-0"></li>
+          <li class="relative my-4 lg:my-0">
             <a
               href="#"
-              class="rounded-full border-2 border-black px-8 py-4 font-medium"
+              class="rounded-full border-2 border-black px-4 py-2 text-sm font-medium lg:px-8 lg:py-4 lg:text-base"
               >Umów wizytę</a
             >
           </li>
-          <li class="flex w-32 justify-between">
+          <li class="flex lg:justify-between lg:w-32">
             <a
               href="#"
-              class="flex h-12 w-12 items-center justify-center rounded-full border-2 border-supernova"
+              class="relative mx-2 flex h-10 w-10 items-center justify-center rounded-full border-2 border-supernova lg:h-12 lg:w-12"
             >
-              <img src="/images/fb.svg" alt="Facebook" />
+              <div
+                class="absolute h-full w-full rounded-full bg-black opacity-20"
+              ></div>
+              <img src="/images/fb.svg" alt="Facebook" class="absolute" />
             </a>
             <a
               href="#"
-              class="flex h-12 w-12 items-center justify-center rounded-full border-2 border-supernova"
+              class="relative mx-2 flex h-10 w-10 items-center justify-center rounded-full border-2 border-supernova lg:h-12 lg:w-12"
             >
-              <img src="/images/insta.svg" alt="Instagram" />
+              <div
+                class="absolute h-full w-full rounded-full bg-black opacity-20"
+              ></div>
+              <img src="/images/insta.svg" alt="Instagram" class="absolute" />
             </a>
           </li>
         </ul>
       </nav>
-      <div class="relative top-[15%]">
+      <div class="relative top-[15%] hidden">
         <div class="drop-shadow-[0_4px_4px_rgba(22,31,54,0.75)]">
           <span class="text-6xl leading-[5rem]"
             >Odzyskaj balans<br />i poczuj się GUT</span
