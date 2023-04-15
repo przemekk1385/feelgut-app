cf0bbb1a165907bf3fa3a14aa05c4287a0dd6d35
Przemysław Kaliś <przemek.kalis@gmail.com>
2023-04-15 13:03:21 +0200

    Apply final content

---
+++ components/VAbout.vue
@@ -0,0 +1,95 @@
+<template>
+  <div class="grid grid-cols-1 bg-como text-white lg:grid-cols-2 min-w-[23rem]">
+    <div>
+      <div class="mb-6 px-6 lg:hidden">
+        <div
+          class="text-6xl font-bold leading-snug md:text-8xl md:leading-snug"
+        >
+          <span class="opacity-30"> O mnie </span>
+          <div
+            class="relative h-[2px] w-1/2 bg-gradient-to-r from-supernova via-sahara to-mimosa lg:-left-48"
+          ></div>
+        </div>
+      </div>
+      <div class="aspect-[2/1] lg:aspect-[3/4] xl:aspect-square">
+        <img
+          src="/images/about.jpg"
+          alt="Dariusz Gut - zdjęcie"
+          class="h-full w-full object-cover object-top"
+        />
+      </div>
+    </div>
+    <div
+      class="flex min-h-[36rem] flex-col p-6 sm:min-h-[24rem] md:min-h-[20rem] lg:pr-28 xl:max-w-[45rem] xl:pr-36"
+    >
+      <div class="mb-6 hidden lg:block">
+        <div
+          class="text-6xl font-bold leading-snug md:text-8xl md:leading-snug"
+        >
+          <span class="opacity-30"> O mnie </span>
+          <div
+            class="relative h-[2px] w-[29rem] bg-gradient-to-r from-supernova via-sahara to-mimosa lg:-left-48"
+          ></div>
+        </div>
+      </div>
+      <div class="flex flex-col flex-grow justify-around font-lato">
+      <p>
+        Cześć, nazywam się Darek Gut. Jestem dyplomowanym masażystą, a z
+        wykształcenia absolwentem Wydziału wychowania fizycznego AWF w Krakowie.
+        Od kilku lat pasjonuję się sztuką masażu, czego efektem są licznie
+        ukończone specjalistyczne szkolenia z wielu technik oraz metod masażu.
+      </p>
+      <p>
+        Największą satysfakcję sprawia mi pomoc drugiemu człowiekowi w
+        dochodzeniu do pełni sprawności, likwidowaniu dolegliwości bólowych, a
+        także pozbywaniu się stresu, z którym zmagają się moi klienci.
+      </p>
+      <p>
+        Wykonuję zarówno masaże terapeutyczne jak i relaksacyjne. Pracuję
+        również jako instruktor m.in. masażu klasycznego i balijskiego w
+        Akademii Sztuki Masażu Menos.
+      </p>
+      <p>
+        Czas wolny lubię spędzać aktywnie m.in. na siłowni czy też startując w
+        biegach długodystansowych. Lubię także piesze wędrówki górskie, a od
+        dziecka pasjonuję się piłką nożną, którą wiele lat uprawiałem.
+      </p>
+      </div>
+    </div>
+
+    <!-- <div class="max-w-[45rem] px-8 pt-16 pb-24 pr-6 md:pr-20 lg:pr-28 xl:pr-36 flex flex-col">
+      <div>
+        <span class="text-6xl font-bold text-white opacity-30 md:text-8xl"
+          >O mnie</span
+        >
+        <div
+          class="relative -left-48 w-[30rem] mt-4 h-[2px] bg-gradient-to-r from-supernova via-sahara to-mimosa"
+        ></div>
+      </div>
+      <div class="flex-grow mt-16 justify-between flex flex-col text-white">
+        <p>
+          Cześć, nazywam się Darek Gut. Jestem dyplomowanym masażystą, a z
+          wykształcenia absolwentem Wydziału wychowania fizycznego AWF w
+          Krakowie. Od kilku lat pasjonuję się sztuką masażu, czego efektem są
+          licznie ukończone specjalistyczne szkolenia z wielu technik oraz metod
+          masażu.
+        </p>
+        <p>
+          Największą satysfakcję sprawia mi pomoc drugiemu człowiekowi w
+          dochodzeniu do pełni sprawności, likwidowaniu dolegliwości bólowych, a
+          także pozbywaniu się stresu, z którym zmagają się moi klienci.
+        </p>
+        <p>
+          Wykonuję zarówno masaże terapeutyczne jak i relaksacyjne. Pracuję
+          również jako instruktor m.in. masażu klasycznego i balijskiego w
+          Akademii Sztuki Masażu Menos.
+        </p>
+        <p>
+          Czas wolny lubię spędzać aktywnie m.in. na siłowni czy też startując w
+          biegach długodystansowych. Lubię także piesze wędrówki górskie, a od
+          dziecka pasjonuję się piłką nożną, którą wiele lat uprawiałem.
+        </p>
+      </div>
+    </div> -->
+  </div>
+</template>
