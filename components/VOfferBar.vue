a420373ef3bd9e45abb662febaf11bc3fd601741
Przemysław Kaliś <przemek.kalis@gmail.com>
2023-04-18 21:52:38 +0200

    Create offer bar and bar button components

---
+++ components/VOfferBar.vue
@@ -0,0 +1,25 @@
+<template>
+  <div :class="`bg-${props.bgColor}`">
+    <div
+        class="mx-auto max-w-[90rem] py-12 px-6 md:px-20 lg:px-28 xl:px-36"
+    >
+      <div class="flex flex-col sm:flex-row items-center justify-around gap-6">
+        <div :class="`text-${props.textColor}`" class="text-xl sm:text-2xl lg:text-3xl font-medium"><slot></slot></div>
+        <div><slot name="button"></slot></div>
+      </div>
+    </div>
+  </div>
+</template>
+
+<script setup lang="ts">
+const props = withDefaults(
+  defineProps<{
+    bgColor: string;
+    textColor: string;
+  }>(),
+  {
+    bgColor: "white",
+    textColor: "black",
+  }
+);
+</script>
---
+++ components/VOfferBarButton.vue
@@ -0,0 +1,29 @@
+<template>
+  <a
+    :href="props.to"
+    class="mx-auto inline-block rounded-full bg-gradient-to-r from-supernova via-sahara to-mimosa p-[2px] font-medium"
+  >
+    <div
+      :class="`bg-${props.bgColor} text-${props.textColor}`"
+      class="rounded-full px-12 py-4 lg:px-16 xl:py-6 xl:px-20"
+    >
+      <div class="text-xl font-medium sm:text-2xl lg:text-3xl">
+        <slot></slot>
+      </div>
+    </div>
+  </a>
+</template>
+
+<script setup lang="ts">
+const props = withDefaults(
+  defineProps<{
+    bgColor: string;
+    textColor: string;
+    to: string;
+  }>(),
+  {
+    bgColor: "white",
+    textColor: "black",
+  }
+);
+</script>
