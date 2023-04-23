e262102f7c8196ed22dd5b4b71c3f318e07908a5
Przemysław Kaliś <przemek.kalis@gmail.com>
2023-04-23 21:22:50 +0200

    Refactorize section

--- components/VAbout.vue
+++ components/VAbout.vue
@@ -1,16 +1,8 @@
 <template>
-  <div class="grid grid-cols-1 bg-como text-white lg:grid-cols-2">
-    <div>
-      <div class="mb-6 px-6 lg:hidden">
-        <div
-          class="text-6xl font-bold leading-snug md:text-8xl md:leading-snug"
-        >
-          <span class="opacity-30"> O mnie </span>
-          <div
-            class="relative h-[2px] w-1/2 bg-gradient-to-r from-supernova via-sahara to-mimosa lg:-left-48"
-          ></div>
-        </div>
-      </div>
+  <div
+    class="flex flex-col-reverse bg-como text-white lg:max-h-[42rem] lg:flex-row xl:max-h-[44rem]"
+  >
+    <div class="lg:flex-1 lg:overflow-hidden">
       <div class="aspect-[2/1] lg:aspect-[3/4] xl:aspect-square">
         <img
           src="/images/about.jpg"
@@ -19,78 +11,43 @@
         />
       </div>
     </div>
-    <div
-      class="flex min-h-[36rem] flex-col p-6 sm:min-h-[24rem] md:min-h-[20rem] lg:pr-28 xl:max-w-[45rem] xl:pr-36"
-    >
-      <div class="mb-6 hidden lg:block">
-        <div
-          class="text-6xl font-bold leading-snug md:text-8xl md:leading-snug"
-        >
-          <span class="opacity-30"> O mnie </span>
-          <div
-            class="relative h-[2px] w-[29rem] bg-gradient-to-r from-supernova via-sahara to-mimosa lg:-left-48"
-          ></div>
+    <div class="flex flex-1">
+      <div class="flex grow flex-col p-6 lg:pr-28 xl:max-w-[45rem] xl:pr-36">
+        <div class="mb-6">
+          <v-h>
+            <span class="text-white opacity-30"> O mnie </span>
+            <template #under>
+              <v-h-line
+                class="relative sm:w-1/2 lg:-left-40 lg:w-[22rem] xl:-left-48 xl:w-[29rem]"
+              />
+            </template>
+          </v-h>
+        </div>
+        <div class="flex flex-grow flex-col justify-around gap-6 font-lato">
+          <p>
+            Cześć, nazywam się Darek Gut. Jestem dyplomowanym masażystą, a z
+            wykształcenia absolwentem Wydziału Wychowania Fizycznego AWF w
+            Krakowie. Od kilku lat pasjonuję się sztuką masażu, czego efektem są
+            licznie ukończone specjalistyczne szkolenia z wielu technik oraz
+            metod masażu.
+          </p>
+          <p>
+            Największą satysfakcję sprawia mi pomoc drugiemu człowiekowi w
+            dochodzeniu do pełni sprawności, likwidowaniu dolegliwości bólowych,
+            a także pozbywaniu się stresu, z którym zmagają się moi klienci.
+          </p>
+          <p>
+            Wykonuję zarówno masaże terapeutyczne jak i relaksacyjne. Pracuję
+            również jako instruktor m.in. masażu klasycznego i balijskiego w
+            Akademii Sztuki Masażu Menos.
+          </p>
+          <p>
+            Czas wolny lubię spędzać aktywnie m.in. na siłowni czy też startując
+            w biegach długodystansowych. Lubię także piesze wędrówki górskie, a
+            od dziecka pasjonuję się piłką nożną, którą wiele lat uprawiałem.
+          </p>
         </div>
       </div>
-      <div class="flex flex-grow flex-col justify-around font-lato">
-        <p>
-          Cześć, nazywam się Darek Gut. Jestem dyplomowanym masażystą, a z
-          wykształcenia absolwentem Wydziału Wychowania Fizycznego AWF w
-          Krakowie. Od kilku lat pasjonuję się sztuką masażu, czego efektem są
-          licznie ukończone specjalistyczne szkolenia z wielu technik oraz metod
-          masażu.
-        </p>
-        <p>
-          Największą satysfakcję sprawia mi pomoc drugiemu człowiekowi w
-          dochodzeniu do pełni sprawności, likwidowaniu dolegliwości bólowych, a
-          także pozbywaniu się stresu, z którym zmagają się moi klienci.
-        </p>
-        <p>
-          Wykonuję zarówno masaże terapeutyczne jak i relaksacyjne. Pracuję
-          również jako instruktor m.in. masażu klasycznego i balijskiego w
-          Akademii Sztuki Masażu Menos.
-        </p>
-        <p>
-          Czas wolny lubię spędzać aktywnie m.in. na siłowni czy też startując w
-          biegach długodystansowych. Lubię także piesze wędrówki górskie, a od
-          dziecka pasjonuję się piłką nożną, którą wiele lat uprawiałem.
-        </p>
-      </div>
     </div>
-
-    <!-- <div class="max-w-[45rem] px-8 pt-16 pb-24 pr-6 md:pr-20 lg:pr-28 xl:pr-36 flex flex-col">
-      <div>
-        <span class="text-6xl font-bold text-white opacity-30 md:text-8xl"
-          >O mnie</span
-        >
-        <div
-          class="relative -left-48 w-[30rem] mt-4 h-[2px] bg-gradient-to-r from-supernova via-sahara to-mimosa"
-        ></div>
-      </div>
-      <div class="flex-grow mt-16 justify-between flex flex-col text-white">
-        <p>
-          Cześć, nazywam się Darek Gut. Jestem dyplomowanym masażystą, a z
-          wykształcenia absolwentem Wydziału wychowania fizycznego AWF w
-          Krakowie. Od kilku lat pasjonuję się sztuką masażu, czego efektem są
-          licznie ukończone specjalistyczne szkolenia z wielu technik oraz metod
-          masażu.
-        </p>
-        <p>
-          Największą satysfakcję sprawia mi pomoc drugiemu człowiekowi w
-          dochodzeniu do pełni sprawności, likwidowaniu dolegliwości bólowych, a
-          także pozbywaniu się stresu, z którym zmagają się moi klienci.
-        </p>
-        <p>
-          Wykonuję zarówno masaże terapeutyczne jak i relaksacyjne. Pracuję
-          również jako instruktor m.in. masażu klasycznego i balijskiego w
-          Akademii Sztuki Masażu Menos.
-        </p>
-        <p>
-          Czas wolny lubię spędzać aktywnie m.in. na siłowni czy też startując w
-          biegach długodystansowych. Lubię także piesze wędrówki górskie, a od
-          dziecka pasjonuję się piłką nożną, którą wiele lat uprawiałem.
-        </p>
-      </div>
-    </div> -->
   </div>
 </template>
