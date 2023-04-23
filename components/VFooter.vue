4a026dc0c92febe925116c9a3f99529b4ac4ad40
Przemysław Kaliś <przemek.kalis@gmail.com>
2023-04-23 21:50:56 +0200

    Rebuild section

---
+++ components/VFooter.vue
@@ -0,0 +1,56 @@
+<template>
+  <div class="bg-como py-12">
+    <div class="mx-auto max-w-[90rem] px-12 lg:px-28 xl:px-36">
+      <div class="flex flex-col justify-center gap-6 md:flex-row md:flex-wrap">
+        <ul class="my-3 flex flex-col gap-y-4 self-center font-lato text-white">
+          <li class="text-center md:text-left"><a href="#">O mnie</a></li>
+          <li class="text-center md:text-left"><a href="#">Usługi</a></li>
+          <li class="text-center md:text-left"><a href="#">Gabinet</a></li>
+          <li class="text-center md:text-left"><a href="#">Umów wizytę</a></li>
+        </ul>
+
+        <div class="h-[2px] w-full bg-white md:h-auto md:w-[2px]">&nbsp;</div>
+
+        <ul class="my-3 flex flex-col gap-y-4 self-center font-lato text-white">
+          <li class="text-center md:text-left">
+            <a href="#">Masaże relaksacyjne</a>
+          </li>
+          <li class="text-center md:text-left">
+            <a href="#">Masaże terapeutyczne</a>
+          </li>
+        </ul>
+
+        <div class="h-[2px] w-full bg-white md:h-auto md:w-[2px]">&nbsp;</div>
+
+        <ul class="my-3 flex flex-col gap-y-4 self-center font-lato text-white">
+          <li class="text-center md:text-left">Telefon: 788 139 475</li>
+          <li class="text-center md:text-left">Mail: gut.darek@gmail.com</li>
+          <li class="text-center md:text-left">
+            Adres: Kolanowska 123, Bochnia
+          </li>
+        </ul>
+
+        <div class="h-[2px] w-full bg-white md:h-auto md:w-[2px]">&nbsp;</div>
+
+        <ul class="my-3 flex flex-row gap-4 self-center md:flex-col">
+          <li>
+            <a
+              href="#"
+              class="flex h-12 w-12 items-center justify-center rounded-full border-2 border-supernova"
+            >
+              <img src="/images/fb.svg" alt="Facebook" />
+            </a>
+          </li>
+          <li>
+            <a
+              href="#"
+              class="flex h-12 w-12 items-center justify-center rounded-full border-2 border-supernova"
+            >
+              <img src="/images/insta.svg" alt="Instagram" />
+            </a>
+          </li>
+        </ul>
+      </div>
+    </div>
+  </div>
+</template>
