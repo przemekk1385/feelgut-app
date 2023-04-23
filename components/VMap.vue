b3afc5e0d432cf6f604f683238b1551dd6957387
Przemysław Kaliś <przemek.kalis@gmail.com>
2023-04-23 21:51:16 +0200

    Adjust margin

--- components/VMap.vue
+++ components/VMap.vue
@@ -1,5 +1,5 @@
 <template>
-  <div class="mx-auto max-w-[90rem] px-6 pt-16 pb-28 md:px-20 lg:px-28 xl:px-36">
+  <div class="mx-auto max-w-[90rem] px-6 py-12 md:px-20 lg:px-28 xl:px-36">
     <iframe
       src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d8225.777529266454!2d20.405140731424474!3d49.95280469113707!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x47163b5a13657f7b%3A0xa9023cac76e75ff0!2sKolanowska%2C%2032-700%20Bochnia!5e0!3m2!1spl!2spl!4v1677368860083!5m2!1spl!2spl"
       loading="lazy"
