ec00d42bccba11153041ebf64505b74ea6a02c42
Przemysław Kaliś <przemek.kalis@gmail.com>
2023-04-23 21:23:27 +0200

    Rebuild sections

--- components/VContact.vue
+++ components/VContact.vue
@@ -5,9 +5,9 @@
     <v-h>
       <span class="text-como opacity-30"> Umów wizytę </span>
       <template #under>
-        <div
-          class="relative mt-6 h-[2px] w-full bg-gradient-to-r from-supernova via-sahara to-mimosa md:left-40 md:w-[15rem] lg:left-44 lg:w-[22rem] xl:left-48 xl:w-[29rem]"
-        ></div>
+        <v-h-line
+          class="relative sm:w-1/2 md:left-40 md:w-[15rem] lg:left-44 lg:w-[22rem] xl:left-48 xl:w-[29rem]"
+        />
       </template>
     </v-h>
     <div class="flex flex-col gap-12 lg:flex-row">
@@ -55,20 +55,20 @@
       <div class="flex flex-col items-center justify-center">
         <div>
           <div
-            class="text-7xl sm:text-8xl font-bold text-sherwood-green opacity-40 lg:text-7xl xl:text-8xl"
+            class="text-7xl font-bold text-sherwood-green opacity-40 sm:text-8xl lg:text-7xl xl:text-8xl"
           >
             Dariusz
           </div>
           <div class="relative -top-4 sm:-top-6 lg:-top-4 xl:-top-6">
             <div
-              class="pl-48 text-7xl sm:pl-60 sm:text-8xl font-bold text-sherwood-green opacity-40 lg:pl-48 lg:text-7xl xl:pl-60 xl:text-8xl"
+              class="pl-48 text-7xl font-bold text-sherwood-green opacity-40 sm:pl-60 sm:text-8xl lg:pl-48 lg:text-7xl xl:pl-60 xl:text-8xl"
             >
               Gut
             </div>
           </div>
         </div>
         <div
-          class="text-6xl sm:text-7xl font-bold text-sherwood-green opacity-40 lg:text-6xl xl:text-7xl"
+          class="text-6xl font-bold text-sherwood-green opacity-40 sm:text-7xl lg:text-6xl xl:text-7xl"
         >
           676 888 930
         </div>
---
+++ components/VMyMassageRoom.vue
@@ -0,0 +1,111 @@
+<template>
+  <div class="bg-como">
+    <div class="mx-auto max-w-[90rem] py-6 px-12">
+      <v-h class="mb-6 block md:hidden">
+        <span class="text-white opacity-30">Mój gabinet</span>
+        <template #under>
+          <v-h-line />
+        </template>
+      </v-h>
+
+      <div class="flex flex-col gap-6 sm:flex-row">
+        <div class="flex grow-[4] basis-0 flex-col gap-6">
+          <div class="aspect-[9/10] overflow-hidden">
+            <img
+              src="/images/my_massage_room/1.jpeg"
+              alt="Zdjęcie ilustracyjne"
+              class="h-full w-full object-cover"
+            />
+          </div>
+          <div class="aspect-[2/1] overflow-hidden bg-red-100">
+            <img
+              src="/images/my_massage_room/2.jpeg"
+              alt="Zdjęcie ilustracyjne"
+              class="h-full w-full object-cover"
+            />
+          </div>
+        </div>
+        <div class="flex grow-[5] basis-0 flex-col justify-end gap-6">
+          <div class="hidden grow items-center md:flex">
+            <v-h class="grow">
+              <span class="text-white opacity-30">Mój gabinet</span>
+              <template #under>
+                <v-h-line
+                  class="relative md:-left-16 md:w-[19rem] lg:-left-20 lg:w-[22rem] xl:-left-24 xl:w-[29rem]"
+                />
+              </template>
+            </v-h>
+          </div>
+
+          <div class="aspect-[2/1] overflow-hidden">
+            <img
+              src="/images/my_massage_room/2.jpeg"
+              alt="Zdjęcie ilustracyjne"
+              class="h-full w-full object-cover"
+            />
+          </div>
+          <div class="aspect-[2/1] overflow-hidden">
+            <img
+              src="/images/my_massage_room/3.jpeg"
+              alt="Zdjęcie ilustracyjne"
+              class="h-full w-full object-cover"
+            />
+          </div>
+        </div>
+      </div>
+    </div>
+
+    <!--
+    <div
+      class="grid grid-cols-1 gap-3 bg-como p-6 md:px-12 lg:grid-cols-2 xl:grid-cols-[repeat(20,_minmax(0,_1fr))]"
+    >
+      <div class="xl:col-span-9">
+        <div class="grid grid-cols-1 gap-3">
+          <div class="aspect-[10/11]">
+            <img
+              src="/images/my_massage_room/1.jpeg"
+              alt="Zdjęcie ilustracyjne"
+              class="h-full w-full object-cover"
+            />
+          </div>
+          <div class="aspect-[2/1]">
+            <img
+              src="/images/my_massage_room/2.jpeg"
+              alt="Zdjęcie ilustracyjne"
+              class="h-full w-full object-cover"
+            />
+          </div>
+        </div>
+      </div>
+      <div class="flex flex-col justify-end xl:col-span-11">
+        <div class="mb-12 flex grow items-center xl:my-0">
+          <div class="inline-block">
+            <span class="text-6xl font-bold text-white opacity-30 md:text-8xl"
+              >Mój gabinet</span
+            >
+            <div
+              class="relative mt-6 h-[2px] w-full bg-gradient-to-r from-supernova via-sahara to-mimosa lg:left-[-15%] lg:w-[85%]"
+            ></div>
+          </div>
+        </div>
+        <div class="grid grid-cols-1 gap-3">
+          <div class="aspect-[2/1]">
+            <img
+              src="/images/my_massage_room/2.jpeg"
+              alt="Zdjęcie ilustracyjne"
+              class="h-full w-full object-cover"
+            />
+          </div>
+          <div class="aspect-[2/1]">
+            <img
+              src="/images/my_massage_room/3.jpeg"
+              alt="Zdjęcie ilustracyjne"
+              class="h-full w-full object-cover"
+            />
+          </div>
+        </div>
+      </div>
+    </div>
+    -->
+  </div>
+</template>
