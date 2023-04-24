29270415d29929a2c862fb914860cda237637159
Przemysław Kaliś <przemek.kalis@gmail.com>
2023-04-24 21:02:08 +0200

    Create page

--- app.vue
+++ app.vue
@@ -1,12 +1,7 @@
+import { NuxtPage } from './.nuxt/components';
+
 <template>
   <div class="min-w-[23rem] font-playfair-display">
-    <v-header />
-    <v-about />
-    <v-offer />
-    <v-reviews />
-    <v-contact />
-    <v-my-massage-room />
-    <v-map />
-    <v-footer />
+    <NuxtPage />
   </div>
 </template>
