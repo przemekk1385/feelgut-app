ffd9afd8ef783af59dcd70dcdc257a9d9a34f6d4
Przemysław Kaliś <przemek.kalis@gmail.com>
2023-04-24 21:15:47 +0200

    wip: 'Cennik' page

---
+++ components/VCommandments.vue
@@ -0,0 +1,73 @@
+<template>
+  <div class="flex flex-col-reverse bg-platinium lg:flex-row">
+    <div class="lg:flex-1 lg:overflow-hidden">
+      <div class="h-full">
+        <img
+          src="/images/commandments.jpg"
+          alt="Grafika ilustracyjna"
+          class="h-full w-full object-cover object-center"
+        />
+      </div>
+    </div>
+    <div class="flex lg:flex-1">
+      <div class="flex grow flex-col p-6 lg:pr-28 xl:max-w-[45rem] xl:pr-36">
+        <div class="mb-6">
+          <v-h>
+            <span class="text-white"> 10 przykazań </span>
+            <template #under>
+              <v-h-line class="relative md:left-16 lg:-left-40 xl:-left-48" />
+            </template>
+          </v-h>
+        </div>
+        <div class="flex flex-grow flex-col justify-around gap-4 font-lato">
+          <span class="text-2xl">Jak przygotować się do masażu?</span>
+          <ul class="flex list-outside list-decimal flex-col gap-4 pl-6">
+            <li>
+              Zadbaj o higienę – pamiętaj o podstawowej higienie, aby zadbać o
+              uczucie świeżości oraz oczyści ciała.
+            </li>
+            <li>
+              Pamiętaj o punktualności – szanujmy swój czas, dlatego proszę o
+              punktualne przybycie na umówiony zabieg lub informację najpóźniej
+              na kilka godzin przed o rezygnacji z terminu.
+            </li>
+            <li>
+              Zapoznaj się z przeciwwskazaniami do masażu – istnieją
+              dolegliwości i schorzenia, które są przeciwwskazaniami do
+              konkretnego masażu. Podane są przy opisie każdego zabiegu.
+            </li>
+            <li>
+              Nie objadaj się przed masażem – pamiętaj, aby ostatni duży posiłek
+              przed przystąpieniem do zabiegu miał miejsce co najmniej 2h przed
+              masażem. Lekkie przekąski nie będą stanowiły dyskomfortu.
+            </li>
+            <li>
+              Nie nakładaj makijażu wybierając się na masaż twarzy – najlepiej
+              pojawić się bez, a jeśli nie ma możliwości wcześniejszego zmycia,
+              przygotuj się na demakijaż.
+            </li>
+            <li>
+              Poinformuj o alergiach na różne składniki – jeśli masz świadomość
+              o uczuleniu na konkretne produkty czy składniki kosmetyków, proszę
+              poinformuj mnie o tym przed zabiegiem.
+            </li>
+            <li>
+              Przygotuj spinki do włosów – jeśli posiadasz długie włosy warto
+              pamiętać o spięciu ich przed samym zabiegiem, aby nie opadały na
+              plecy i ramiona w trakcie wykonywania masażu.
+            </li>
+            <li>
+              Ściągnij biżuterię – proszę pamiętać, że są to elementy, które
+              również przeszkadzają podczas wykonywania masażu dlatego
+              zdejmujemy je przed zabiegiem.
+            </li>
+            <li>
+              Zaufaj mi – masaż to moja praca i pasja w jednym, dlatego ważne
+              jest wzajemne zaufanie na linii klient-masażysta.
+            </li>
+          </ul>
+        </div>
+      </div>
+    </div>
+  </div>
+</template>
---
+++ pages/cennik.vue
@@ -0,0 +1,8 @@
+<template>
+  <v-nav />
+  <main>
+    <v-commandments />
+    <v-pricing />
+  </main>
+  <v-footer />
+</template>
