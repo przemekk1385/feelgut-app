578fead9f2b8f6f40ab4c26c9b86b9d2725965c0
Przemysław Kaliś <przemek.kalis@gmail.com>
2023-05-01 21:18:09 +0200

    Create pricing component

---
+++ components/VPricing.vue
@@ -0,0 +1,41 @@
+<template>
+  <div class="mx-auto my-24 max-w-[90rem] px-6 md:px-20 lg:px-28 xl:px-36">
+    <v-h>
+      <span class="text-como opacity-30"> Cennik </span>
+      <template #under>
+        <v-h-line class="relative md:left-6 lg:left-9 xl:left-12" />
+      </template>
+    </v-h>
+    <div class="mt-12 pl-0 lg:pl-16">
+      <v-accordion
+        v-for="(
+          { title, price, image, text, indications, contraindications }, i
+        ) in items"
+        :key="title"
+        :dark="i % 2 === 1"
+        :image="image"
+        :title="title"
+        :price="price"
+      >
+        {{ text }}
+        <template #indications>
+          <ul class="list-disc pl-6">
+            <li v-for="item in indications">{{ item }}</li>
+          </ul>
+        </template>
+        <template #contraindications>
+          <ul class="list-disc pl-6">
+            <li v-for="item in contraindications">{{ item }}</li>
+          </ul>
+        </template>
+      </v-accordion>
+    </div>
+  </div>
+</template>
+
+<script setup lang="ts">
+import type { Massage } from "~~/types";
+
+const items: Ref<Massage[]> = ref([]);
+items.value = (await queryContent("offer").find()) as unknown as Massage[];
+</script>
