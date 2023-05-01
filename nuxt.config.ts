551d14afdfa72f3ff27357b9f834749aeae13bfc
Przemysław Kaliś <przemek.kalis@gmail.com>
2023-05-01 21:15:59 +0200

    Add content package

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,6 +1,12 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
-  modules: ["@nuxtjs/tailwindcss"],
+  modules: ["@nuxt/content", "@nuxtjs/tailwindcss"],
+  postcss: {
+    plugins: {
+      tailwindcss: {},
+      autoprefixer: {},
+    },
+  },
   tailwindcss: {
     // options
   },
