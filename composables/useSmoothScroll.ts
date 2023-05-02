ddca20b4aada9ef4d277ea7dd575be8e6bc51db6
Przemysław Kaliś <przemek.kalis@gmail.com>
2023-05-02 21:27:36 +0200

    Format

--- composables/useSmoothScroll.ts
+++ composables/useSmoothScroll.ts
@@ -2,7 +2,8 @@ export function useSmoothScroll() {
   const route = useRoute();
 
   const smoothScroll: any = inject("smoothScroll");
-  const delay = (timeout: number): Promise<void> => new Promise(resolve => setTimeout(resolve, timeout));
+  const delay = (timeout: number): Promise<void> =>
+    new Promise((resolve) => setTimeout(resolve, timeout));
 
   watch(
     () => route.query,
@@ -13,7 +14,7 @@ export function useSmoothScroll() {
 
         const el: HTMLElement | null = document.querySelector(`[id="${goto}"]`);
         if (el) {
-          navigateTo({ path: route.path, query: { category }});
+          navigateTo({ path: route.path, query: { category } });
           delay(100);
           smoothScroll({
             scrollTo: el,
