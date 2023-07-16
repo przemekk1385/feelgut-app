cfb141c5a45ec9ad6e07c08f0b00e2b7df9fe520
Przemysław Kaliś <przemek.kalis@gmail.com>
2023-07-16 10:49:29 +0200

    Add title

--- components/VMap.vue
+++ components/VMap.vue
@@ -4,6 +4,7 @@
       src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d2561.2575640637956!2d19.943799377695463!3d50.06273787152083!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x47165b10d66f6c55%3A0x605a3ce262a9f38e!2sAkademia%20Sztuki%20Masa%C5%BCu%20MENOS!5e0!3m2!1spl!2spl!4v1684618857956!5m2!1spl!2spl"
       loading="lazy"
       referrerpolicy="no-referrer-when-downgrade"
+      title="Mapa"
       class="aspect-[2/1] h-full w-full border-0 md:aspect-[4/1]"
     ></iframe>
   </div>
