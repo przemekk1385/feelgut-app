eaba5db3f689458b4a6256f546e8a9ac1e038608
Przemysław Kaliś <przemek.kalis@gmail.com>
2024-11-08 19:46:35 +0100

    refactor: add photo to contact section

--- components/VContact.vue
+++ components/VContact.vue
@@ -1,88 +1,91 @@
 <template>
   <div
     id="kontakt"
-    class="mx-auto max-w-[90rem] px-6 pb-28 pt-16 md:px-20 lg:px-28 xl:px-36"
+    class="flex flex-col-reverse gap-12 lg:max-h-[50rem] lg:flex-row"
   >
-    <v-h>
-      <span class="text-como opacity-30"> Umów wizytę </span>
-      <template #under>
-        <v-h-line class="relative md:left-40 lg:left-44 xl:left-48" />
-      </template>
-    </v-h>
-    <div class="flex flex-col gap-12 lg:flex-row">
-      <FormKit
-        id="contact-form"
-        :actions="false"
-        type="form"
-        form-class="mt-24 flex grow flex-col gap-y-6"
-        @submit="handleSubmit"
-      >
-        <FormKit
-          type="text"
-          name="name"
-          placeholder="Imię i nazwisko"
-          validation="required"
-          validation-visibility="submit"
-          :validation-messages="{ required: 'To pole jest wymagane.' }"
-        />
-        <FormKit
-          type="text"
-          name="email"
-          placeholder="Adres email"
-          validation="required|email"
-          validation-visibility="submit"
-          :validation-messages="{ required: 'To pole jest wymagane.' }"
-        />
-        <FormKit
-          type="textarea"
-          name="text"
-          placeholder="Treść wiadomości"
-          validation="required"
-          input-class="aspect-[5/2] w-full"
-          validation-visibility="submit"
-          :validation-messages="{ required: 'To pole jest wymagane.' }"
-        />
-        <FormKit
-          label="consent"
-          type="checkbox"
-          name="consent"
-          validation="accepted"
-          wrapper-class="flex items-center gap-x-4"
-          inner-class="flex"
-          validation-visibility="submit"
-          :validation-messages="{
-            accepted:
-              'Proszę zaakceptować zgodę na przetwarzanie danych osobowych.',
-          }"
-        >
-          <template #label>
-            <div class="font-lato text-xs font-light">
-              Wyrażam zgodę na przetwarzanie danych osobowych -
-              <NuxtLink href="/polityka-prywatnosci" class="uppercase underline"
-                >czytaj więcej</NuxtLink
-              >
-            </div>
-          </template>
-        </FormKit>
-        <FormKit type="submit" outer-class="flex justify-end">
-          <span class="opacity-40">wyślij</span>
-        </FormKit>
-      </FormKit>
-      <div
-        class="flex flex-col items-center justify-center font-bold text-[#073F31] opacity-40"
-      >
-        <div class="text-7xl font-bold sm:text-8xl lg:text-7xl xl:text-8xl">
-          <div>Dariusz</div>
-          <div class="relative -top-4 sm:-top-6 lg:-top-4 xl:-top-6">
-            <div class="pl-48 sm:pl-60 lg:pl-48 xl:pl-60">Gut</div>
-          </div>
+    <div class="flex">
+      <div class="flex grow flex-col p-6 lg:grow-0 lg:pl-28 xl:pl-36">
+        <div class="mb-6 lg:min-w-[36.5rem]">
+          <v-h>
+            <h2 class="text-como opacity-30">Umów wizytę</h2>
+            <template #under>
+              <v-h-line class="relative md:left-40 lg:left-44 xl:left-48" />
+            </template>
+          </v-h>
         </div>
-        <div class="text-6xl sm:text-7xl lg:text-6xl xl:text-7xl">
-          788 139 475
+        <div class="flex grow flex-col justify-around gap-4 font-lato">
+          <FormKit
+            id="contact-form"
+            :actions="false"
+            type="form"
+            form-class="mt-12 flex grow flex-col gap-y-6"
+            @submit="handleSubmit"
+          >
+            <FormKit
+              type="text"
+              name="name"
+              placeholder="Imię i nazwisko"
+              validation="required"
+              validation-visibility="submit"
+              :validation-messages="{ required: 'To pole jest wymagane.' }"
+            />
+            <FormKit
+              type="text"
+              name="email"
+              placeholder="Adres email"
+              validation="required|email"
+              validation-visibility="submit"
+              :validation-messages="{ required: 'To pole jest wymagane.' }"
+            />
+            <FormKit
+              type="textarea"
+              name="text"
+              placeholder="Treść wiadomości"
+              validation="required"
+              input-class="aspect-[5/2] w-full"
+              validation-visibility="submit"
+              :validation-messages="{ required: 'To pole jest wymagane.' }"
+            />
+            <FormKit
+              label="consent"
+              type="checkbox"
+              name="consent"
+              validation="accepted"
+              wrapper-class="flex items-center gap-x-4"
+              inner-class="flex"
+              validation-visibility="submit"
+              :validation-messages="{
+                accepted:
+                  'Proszę zaakceptować zgodę na przetwarzanie danych osobowych.',
+              }"
+            >
+              <template #label>
+                <div class="font-lato text-xs font-light">
+                  Wyrażam zgodę na przetwarzanie danych osobowych -
+                  <NuxtLink
+                    href="/polityka-prywatnosci"
+                    class="uppercase underline"
+                    >czytaj więcej</NuxtLink
+                  >
+                </div>
+              </template>
+            </FormKit>
+            <FormKit type="submit" outer-class="flex justify-end">
+              <span class="opacity-40">wyślij</span>
+            </FormKit>
+          </FormKit>
         </div>
       </div>
     </div>
-    <VInfo v-model="show" :is-error="isError">{{ message }}</VInfo>
+    <div class="flex grow lg:overflow-hidden">
+      <div class="aspect-square size-full lg:aspect-[2/3]">
+        <img
+          src="/images/contact.jpg"
+          alt="Kontakt - grafika ilustracyjna"
+          class="size-full object-cover object-bottom"
+        />
+      </div>
+    </div>
   </div>
 </template>
 
