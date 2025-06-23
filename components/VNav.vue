4d714865b1c5f52b149e70762bfc5197488fb237
Przemysław Kaliś <przemek.kalis@gmail.com>
2025-06-23 21:48:30 +0200

    chore: use hardcoded social media icons

--- components/VNav.vue
+++ components/VNav.vue
@@ -14,14 +14,38 @@
             <NuxtLink :to="to"> {{ description }} </NuxtLink>
           </li>
           <li class="flex gap-x-4">
+            <!--!Font Awesome Free 6.7.2 by @fontawesome - https://fontawesome.com License - https://fontawesome.com/license/free Copyright 2025 Fonticons, Inc.-->
+
             <v-social-button
-              v-for="{ alt, src, href } in social"
-              :key="href"
-              :alt="alt"
-              :src="src"
-              :href="href"
               class="size-10"
-            />
+              href="https://www.facebook.com/masazysta.feelgut"
+            >
+              <svg
+                xmlns="http://www.w3.org/2000/svg"
+                class="size-full"
+                fill="currentColor"
+                viewBox="0 0 320 512"
+              >
+                <path
+                  d="M80 299.3V512H196V299.3h86.5l18-97.8H196V166.9c0-51.7 20.3-71.5 72.7-71.5c16.3 0 29.4 .4 37 1.2V7.9C291.4 4 256.4 0 236.2 0C129.3 0 80 50.5 80 159.4v42.1H14v97.8H80z"
+                />
+              </svg>
+            </v-social-button>
+            <v-social-button
+              class="size-10"
+              href="https://www.linkedin.com/in/dariusz-gut/"
+            >
+              <svg
+                xmlns="http://www.w3.org/2000/svg"
+                class="size-full"
+                fill="currentColor"
+                viewBox="0 0 448 512"
+              >
+                <path
+                  d="M100.3 448H7.4V148.9h92.9zM53.8 108.1C24.1 108.1 0 83.5 0 53.8a53.8 53.8 0 0 1 107.6 0c0 29.7-24.1 54.3-53.8 54.3zM447.9 448h-92.7V302.4c0-34.7-.7-79.2-48.3-79.2-48.3 0-55.7 37.7-55.7 76.7V448h-92.8V148.9h89.1v40.8h1.3c12.4-23.5 42.7-48.3 87.9-48.3 94 0 111.3 61.9 111.3 142.3V448z"
+                />
+              </svg>
+            </v-social-button>
           </li>
         </ul>
       </v-drawer>
@@ -41,14 +65,38 @@
           <NuxtLink :to="to"> {{ description }} </NuxtLink>
         </li>
         <li class="flex gap-x-6">
+          <!--!Font Awesome Free 6.7.2 by @fontawesome - https://fontawesome.com License - https://fontawesome.com/license/free Copyright 2025 Fonticons, Inc.-->
+
           <v-social-button
-            v-for="{ alt, src, href } in social"
-            :key="href"
-            :alt="alt"
-            :src="src"
-            :href="href"
             class="size-12 drop-shadow-[5px_5px_10px_rgba(0,0,0,.25)]"
-          />
+            href="https://www.facebook.com/masazysta.feelgut"
+          >
+            <svg
+              xmlns="http://www.w3.org/2000/svg"
+              class="size-full"
+              fill="currentColor"
+              viewBox="0 0 320 512"
+            >
+              <path
+                d="M80 299.3V512H196V299.3h86.5l18-97.8H196V166.9c0-51.7 20.3-71.5 72.7-71.5c16.3 0 29.4 .4 37 1.2V7.9C291.4 4 256.4 0 236.2 0C129.3 0 80 50.5 80 159.4v42.1H14v97.8H80z"
+              />
+            </svg>
+          </v-social-button>
+          <v-social-button
+            class="size-12 drop-shadow-[5px_5px_10px_rgba(0,0,0,.25)]"
+            href="https://www.linkedin.com/in/dariusz-gut/"
+          >
+            <svg
+              xmlns="http://www.w3.org/2000/svg"
+              class="size-full"
+              fill="currentColor"
+              viewBox="0 0 448 512"
+            >
+              <path
+                d="M100.3 448H7.4V148.9h92.9zM53.8 108.1C24.1 108.1 0 83.5 0 53.8a53.8 53.8 0 0 1 107.6 0c0 29.7-24.1 54.3-53.8 54.3zM447.9 448h-92.7V302.4c0-34.7-.7-79.2-48.3-79.2-48.3 0-55.7 37.7-55.7 76.7V448h-92.8V148.9h89.1v40.8h1.3c12.4-23.5 42.7-48.3 87.9-48.3 94 0 111.3 61.9 111.3 142.3V448z"
+              />
+            </svg>
+          </v-social-button>
         </li>
       </ul>
     </div>
@@ -62,14 +110,5 @@ const items: Ref<{ description: string; to: string }[]> = ref([
 	{ description: "Cennik", to: "/cennik?goto=cennik" },
 	{ description: "Masaż dla firm", to: "/masaz-dla-firm?goto=masaz-dla-firm" },
 	{ description: "Umów wizytę", to: "/?goto=kontakt" },
-]);
-
-const social: Ref<{ alt: string; src: string; href: string }[]> = ref([
-	{
-		alt: "Facebook",
-		src: "/images/fb.svg",
-		href: "https://www.facebook.com/masazysta.feelgut",
-	},
-	// { alt: "Instagram", src: "/images/insta.svg", href: "#" },
 ]);
 </script>
