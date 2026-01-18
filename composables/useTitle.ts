58fa61daf84c37b6bdc0c8337c0470540c176eff
Przemysław Kaliś <przemek.kalis@gmail.com>
2026-01-18 15:37:55 +0100

    chore: extend pageTitle type with undefined

--- composables/useTitle.ts
+++ composables/useTitle.ts
@@ -1,7 +1,7 @@
 export function useTitle() {
 	const siteTitle = "FeelGUT";
 
-	const titleTemplate = (pageTitle: string): string =>
+	const titleTemplate = (pageTitle: string | undefined): string =>
 		pageTitle ? `${pageTitle} :: ${siteTitle}` : siteTitle;
 
 	return { titleTemplate };
